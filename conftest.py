d74279a60c63b7036982baece99ae2c2933b6b8a
ewhite1999 <ewhite1999@gmail.com>
2022-03-22 15:39:13 +0000

    add in fake data

--- conftest.py
+++ conftest.py
@@ -2,13 +2,33 @@ import pytest
 import app
 from controllers import pokemon
 
+
 @pytest.fixture
 def api(monkeypatch):
-    test_pokemon = [
-        {'id': 1, 'name': 'Test Cat 1', 'age': 7},
-        {'id': 2, 'name': 'Test Cat 2', 'age': 4}
-    ]
-    # This is assuming the data is being imported and saved as pokemon inside of controllers/pokemon.py
+    test_pokemon = [{"id": 1,
+                     "name": "Bulbasaur",
+                     "type": ["Grass",
+                              "Poison"],
+                     "description": "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun’s rays, the seed grows progressively larger.",
+                     "height": "0.7 m",
+                     "weight": "6.9 kg"},
+                    {"id": 2,
+                     "name": "Ivysaur",
+                     "type": ["Grass",
+                              "Poison"],
+                     "description": "There is a bud on this Pokémon’s back. To support its weight, Ivysaur’s legs and trunk grow thick and strong. If it starts spending more time lying in the sunlight, it’s a sign that the bud will bloom into a large flower soon.",
+                     "height": "1 m",
+                     "weight": "13 kg"},
+                    {"id": 3,
+                     "name": "Venusaur",
+                     "type": ["Grass",
+                              "Poison"],
+                     "description": "There is a large flower on Venusaur’s back. The flower is said to take on vivid colors if it gets plenty of nutrition and sunlight. The flower’s aroma soothes the emotions of people.",
+                     "height": "2 m",
+                     "weight": "100 kg"},
+                    ]
+    # This is assuming the data is being imported and saved as pokemon inside
+    # of controllers/pokemon.py
     monkeypatch.setattr(pokemon, "pokemon", test_pokemon)
     api = app.app.test_client()
     return api
