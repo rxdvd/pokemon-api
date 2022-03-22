2792fab669d11e872ee3809147614e9ee1722d04
ewhite1999 <ewhite1999@gmail.com>
2022-03-22 16:10:16 +0000

    add post route

--- test_app.py
+++ test_app.py
@@ -1,4 +1,5 @@
 import json
+from wsgiref import headers
 
 
 class TestAPICases():
@@ -12,7 +13,7 @@ class TestAPICases():
         assert len(res.json) == 3
         assert res.json[2]['name'] == 'Venusaur'
 
-    def test_get_pokemon(self,api):
+    def test_get_pokemon(self, api):
         res = api.get('/pokemon/2')
         assert res.status == '200 OK'
         assert res.json['name'] == 'Ivysaur'
@@ -21,3 +22,33 @@ class TestAPICases():
         res = api.get('/pokemon/4')
         assert res.status == '404 NOT FOUND'
         assert "no pokemon with id 4" in res.json['message']
+
+    def test_post_pokemon(self, api):
+        mock_data = json.dumps(
+            {
+                "name": "test pokemon",
+                "type": [
+                    "fire", ],
+                "description": "A massive test pokemon",
+                "height": "122 m",
+                "weight": "1000 kg"},
+        )
+        mock_headers = {'Content-Type': 'application/json'}
+        res = api.post('/pokemon', data=mock_data, headers=mock_headers)
+        assert res.status == '201 CREATED'
+        assert res.json['id'] == 4
+        assert res.json['name'] == 'test pokemon'
+
+    def test_post_pokemon(self, api):
+        mock_data = json.dumps(
+            {
+                "type": [
+                    "fire", ],
+                "description": "A massive test pokemon",
+                "height": "122 m",
+                "weight": "1000 kg"},
+        )
+        mock_headers = {'Content-Type': 'application/json'}
+        res = api.post('/pokemon', data=mock_data, headers=mock_headers)
+        assert res.status == '400 BAD REQUEST'
+        assert 'needs a name' in res.json['message']
