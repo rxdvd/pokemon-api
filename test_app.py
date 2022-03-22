3b416e5920b87c5416f6e20fd2523253dba02bb5
ewhite1999 <ewhite1999@gmail.com>
2022-03-22 15:53:46 +0000

    add get route test

--- test_app.py
+++ test_app.py
@@ -1,13 +1,23 @@
 import json
 
+
 class TestAPICases():
     def test_home_route(self, api):
         res = api.get('/')
         assert res.status == '200 OK'
-    
+
     def test_index_route(self, api):
         res = api.get('/pokemon')
         assert res.status == '200 OK'
         assert len(res.json) == 3
-        assert res.json[2].name == 'Venusaur'
+        assert res.json[2]['name'] == 'Venusaur'
 
+    def test_get_pokemon(self,api):
+        res = api.get('/pokemon/2')
+        assert res.status == '200 OK'
+        assert res.json['name'] == 'Ivysaur'
+
+    def test_get_pokemon_404(self, api):
+        res = api.get('/pokemon/4')
+        assert res.status == '404 NOT FOUND'
+        assert "no pokemon with id 4" in res.json['message']
