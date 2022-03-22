e42270a09f077c108852272fcccef9401ae7ec01
rxdvd <rxdvd0@gmail.com>
2022-03-22 16:11:23 +0000

    fix missing methods and if statements

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from controllers import pokemon
+from controllers import pokemons
 from werkzeug import exceptions
 
 app = Flask(__name__)
@@ -10,26 +10,26 @@ CORS(app)
 def home():
     return "Welcome to the Pokemon API. See https://github.com/rxdvd/pokemon-api for more info."
 
-@app.route('/pokemon')
+@app.route('/pokemon', methods=['GET', 'POST'])
 def pokemon_route():
     response = {'message': 'Error: Method Not Allowed'}
     status_code = 405
-    if request.method is 'GET':
-        response, status_code = pokemon.index(request)
-    elif request.method is 'POST':
-        response, status_code = pokemon.create(request)
+    if request.method == 'GET':
+        response, status_code = pokemons.index(request)
+    elif request.method == 'POST':
+        response, status_code = pokemons.create(request)
     return jsonify(response), status_code
 
-@app.route('/pokemon/<int:pokemon_id>')
+@app.route('/pokemon/<int:pokemon_id>', methods=['GET', 'PATCH', 'DELETE'])
 def pokemon_id_route(pokemon_id):
     response = {'message': 'Error: Method Not Allowed'}
     status_code = 405
-    if request.method is 'GET':
-        response, status_code = pokemon.show(request, pokemon_id)
-    elif request.method is 'PATCH':
-        response, status_code = pokemon.update(request, pokemon_id)
-    elif request.method is 'DELETE':
-        response, status_code = pokemon.destroy(request, pokemon_id)
+    if request.method == 'GET':
+        response, status_code = pokemons.show(request, pokemon_id)
+    elif request.method == 'PATCH':
+        response, status_code = pokemons.update(request, pokemon_id)
+    elif request.method == 'DELETE':
+        response, status_code = pokemons.destroy(request, pokemon_id)
     return jsonify(response), status_code
 
 @app.errorhandler(exceptions.BadRequest)
